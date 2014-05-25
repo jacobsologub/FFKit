757be5e06fd655711a4320dc78bb42da99479b7a
Jacob Sologub <jacobsologub@gmail.com>
2014-05-24 20:45:40 -0700

    Added initial sources to the project.

---
+++ EXKit/EXKit/core/containers/NSArray+EXKit.h
@@ -0,0 +1,49 @@
+/*
+  ==============================================================================
+ 
+   Copyright (C) 2014 Jacob Sologub
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to
+   deal in the Software without restriction, including without limitation the
+   rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+   sell copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+   IN THE SOFTWARE.
+ 
+  ==============================================================================
+*/
+
+@interface NSArray (EXKit)
+
+/** Returns the lowest index whose corresponding array value is equal to a given 
+    Class.
+ 
+    @see indexOfObjectOfClassType:inRange
+*/
+- (NSUInteger) indexOfObjectOfClassType: (Class) classType;
+
+/** Returns the lowest index within a specified range whose corresponding array 
+    value is equal to a given Class.
+ 
+    @see indexOfObjectOfClassType
+*/
+- (NSUInteger) indexOfObjectOfClassType: (Class) classType inRange: (NSRange) range;
+
+/** Returns a new array that is shuffled version of the receiving array. 
+    
+    @see NSMutableArray#shuffleArray
+*/
+- (NSArray*) shuffledArray;
+
+@end
---
+++ EXKit/EXKit/gui/buttons/UIBarButtonItem+EXKit.m
@@ -0,0 +1,94 @@
+/*
+  ==============================================================================
+ 
+   Copyright (C) 2014 Jacob Sologub
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to
+   deal in the Software without restriction, including without limitation the
+   rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+   sell copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+   IN THE SOFTWARE.
+ 
+  ==============================================================================
+*/
+
+#import "UIBarButtonItem+EXKit.h"
+
+@implementation UIBarButtonItem (EXKit)
+
++ (UIBarButtonItem*) barButtonItemWithImage: (UIImage*) image target: (id) target action: (SEL) action {
+    return [self barButtonItemWithImage: image style: UIBarButtonItemStylePlain target: target action: action];
+}
+
++ (UIBarButtonItem*) barButtonItemWithImage: (UIImage*) image style: (UIBarButtonItemStyle) style target: (id) target action: (SEL) action {
+    UIBarButtonItem* result = [[UIBarButtonItem alloc] initWithImage: image style: style target: target action: action];
+    
+   #if !__has_feature (objc_arc)
+    return [result autorelease];
+   #else
+    return result;
+   #endif
+}
+
++ (UIBarButtonItem*) barButtonItemWithTitle: (NSString*) title target: (id) target action: (SEL) action {
+    return [self barButtonItemWithTitle: title style: UIBarButtonItemStylePlain target: target action: action];
+}
+
++ (UIBarButtonItem*) barButtonItemWithTitle: (NSString*) title style: (UIBarButtonItemStyle) style target: (id) target action: (SEL) action {
+    UIBarButtonItem* result = [[UIBarButtonItem alloc] initWithTitle: title style: style target: target action: action];
+    
+   #if !__has_feature (objc_arc)
+    return [result autorelease];
+   #else
+    return result;
+   #endif
+}
+
++ (UIBarButtonItem*) barButtonSystemItemFixedSpace {
+    return [self barButtonSystemItemFixedSpaceWithWidth: 0.0f];
+}
+
++ (UIBarButtonItem*) barButtonSystemItemFixedSpaceWithWidth: (CGFloat) width {
+    UIBarButtonItem* result = [[UIBarButtonItem alloc] initWithBarButtonSystemItem: UIBarButtonSystemItemFixedSpace target: nil action: nil];
+    result.width = width;
+    
+   #if !__has_feature (objc_arc)
+    return [result autorelease];
+   #else
+    return result;
+   #endif
+}
+
++ (UIBarButtonItem*) barButtonSystemItemFlexibleSpace {
+    UIBarButtonItem* result = [[UIBarButtonItem alloc] initWithBarButtonSystemItem: UIBarButtonSystemItemFlexibleSpace target: nil action: nil];
+    
+   #if !__has_feature (objc_arc)
+    return [result autorelease];
+   #else
+    return result;
+   #endif
+}
+
++ (UIBarButtonItem*) barButtonItemWithCustomView: (UIView*) customView {
+    UIBarButtonItem* result = [[UIBarButtonItem alloc] initWithCustomView: customView];
+    
+   #if !__has_feature (objc_arc)
+    return [result autorelease];
+   #else
+    return result;
+   #endif
+}
+
+@end
