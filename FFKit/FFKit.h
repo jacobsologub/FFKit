c30529c145df0bba1d94491e0222b9ee5533ef11
Jacob Sologub <jacobsologub@gmail.com>
2015-05-09 18:07:50 -0700

    New categorie NSJSONSerialization+FFKit.h.

--- FFKit/FFKit.h
+++ FFKit/FFKit.h
@@ -29,6 +29,7 @@
     #import "core/NSObject+FFKit.h"
     #import "core/containers/NSArray+FFKit.h"
     #import "core/containers/NSDictionary+FFKit.h"
+    #import "core/containers/NSJSONSerialization+FFKit.h"
     #import "core/containers/NSMutableArray+FFKit.h"
     #import "core/gcd/GCD+FFKit.h"
     #import "core/text/NSAttributedString+FFKit.h"
