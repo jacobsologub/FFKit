952379ba0d2d0f46c28b7313c6241af9f49a045a
Jacob Sologub <jacobsologub@gmail.com>
2016-06-03 13:31:44 -0700

    New category NSNotificationCenter+FFKit.

--- FFKit/FFKit.h
+++ FFKit/FFKit.h
@@ -38,6 +38,7 @@
     #import "application/NSBundle+FFKit.h"
     #import "application/FFApplication.h"
     #import "core/NSObject+FFKit.h"
+    #import "core/NSNotificationCenter+FFKit.h"
     #import "core/containers/NSArray+FFKit.h"
     #import "core/containers/NSDictionary+FFKit.h"
     #import "core/containers/NSIndexPath+FFKit.h"
---
+++ FFKit/core/NSNotificationCenter+FFKit.h
@@ -0,0 +1,75 @@
+/*
+  ==============================================================================
+ 
+   Copyright (C) 2016 Jacob Sologub
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to
+   deal in the Software without restriction, including without limitation the
+   rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+   sell copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+   IN THE SOFTWARE.
+ 
+  ==============================================================================
+*/
+
+#import <Foundation/Foundation.h>
+
+@interface NSNotificationCenter (FFKit)
+
+NS_ASSUME_NONNULL_BEGIN
+
+/**	A handy method to add a UIKeyboardWillShowNotification observer instead of 
+    writing out addObserver:selector:name:object.
+ 
+    @see UIKeyboardWillShowNotification, NSNotificationCenter#addObserver:selector:name:object
+*/
++ (void) addUIKeyboardWillShowNotificationObserver: (id) observer selector: (SEL) selector;
+
+/**	A handy method to add a UIKeyboardDidShowNotification observer instead of
+    writing out addObserver:selector:name:object.
+ 
+    @see UIKeyboardDidShowNotification, NSNotificationCenter#addObserver:selector:name:object
+*/
++ (void) addUIKeyboardDidShowNotificationObserver: (id) observer selector: (SEL) selector;
+
+/**	A handy method to add a UIKeyboardWillHideNotification observer instead of
+    writing out addObserver:selector:name:object.
+ 
+    @see UIKeyboardWillHideNotification, NSNotificationCenter#addObserver:selector:name:object
+*/
++ (void) addUIKeyboardWillHideNotificationObserver: (id) observer selector: (SEL) selector;
+
+/**	A handy method to add a UIKeyboardDidHideNotification observer instead of
+    writing out addObserver:selector:name:object.
+ 
+    @see UIKeyboardDidHideNotification, NSNotificationCenter#addObserver:selector:name:object
+*/
++ (void) addUIKeyboardDidHideNotificationObserver: (id) observer selector: (SEL) selector;
+
+/**	A handy method to remove an observer from the default NSNotificationCenter.
+ 
+    @see NSNotificationCenter#removeObserver:
+*/
++ (void) removeObserver: (id) observer;
+
+/**	A handy method to remove an observer from the default NSNotificationCenter.
+ 
+    @see NSNotificationCenter#removeObserver:name:object
+*/
++ (void) removeObserver: (id) observer name: (nullable NSString*) name object: (nullable id) object;
+
+NS_ASSUME_NONNULL_END
+
+@end
